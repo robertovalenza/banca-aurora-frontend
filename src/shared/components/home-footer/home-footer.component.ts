f1f46b90bc4bae08e13f2e8f9c7dfb94b402b4f7
Roberto Valenza <robertovalenza@MacBook-Pro-di-Roberto.local>
2025-09-21 18:36:45 +0200

    feat: implement feedbacks card

--- src/shared/components/home-footer/home-footer.component.ts
+++ src/shared/components/home-footer/home-footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { resultsList } from "../../models/results";
 
 @Component({
-  selector: 'app-home-footer',
+  selector: "app-home-footer",
   imports: [],
-  templateUrl: './home-footer.component.html',
-  styleUrl: './home-footer.component.scss'
+  templateUrl: "./home-footer.component.html",
+  styleUrl: "./home-footer.component.scss",
 })
 export class HomeFooterComponent {
-
+  resultsList = resultsList;
 }
---
+++ src/shared/models/results.ts
@@ -0,0 +1,8 @@
+import { IResult } from "../interfaces/result";
+
+export const resultsList: IResult[] = [
+  { value: "4,25%", description: "Rendimento percentuale annuo" },
+  { value: "2M+", description: "Imprenditori" },
+  { value: "80+", description: "Paesi supportati" },
+  { value: "4.9", description: "Valutazione su App Store e Play Store" },
+];
