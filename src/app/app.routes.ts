01f151c5074a2708de2060495dcd08e82e3aa138
Roberto Valenza <robertovalenza@MacBook-Pro-di-Roberto.local>
2025-09-21 13:26:31 +0200

    feat: add navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("../shared/components/home/home.component").then(
+        (m) => m.HomeComponent
+      ),
+  },
+];
